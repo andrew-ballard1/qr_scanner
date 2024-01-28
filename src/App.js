81fff351a128aafa09a8811b459ead9f43ca34fd
andrew-ballard1 <andrewj.ballard94@gmail.com>
2024-01-28 10:42:14 -0800

    BUTTONS

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ import TakePhotoComponent from './TakePhotoComponent'
 
 
 const Home = () => {
-	return (<nav>
-		<ul>
-			<li>
-				<Link to="/scan-code">Scan Code</Link>
-			</li>
-			<li>
-				<Link to="/take-photo">Take Photo</Link>
-			</li>
-		</ul>
-	</nav>)
+	return (
+		<div style={{display: 'flex', width: 400, justifyContent: 'space-around'}}>
+			<Link style={{height: 50, width: 150, backgroundColor: '#3CF', borderRadius: 5, color: 'white', display: 'flex', textDecoration: 'none', fontWeight: 300, justifyContent: 'center', alignItems: 'center'}} to="/scan-code">Scan Code</Link>
+			<Link style={{height: 50, width: 150, backgroundColor: '#3CF', borderRadius: 5, color: 'white', display: 'flex', textDecoration: 'none', fontWeight: 300, justifyContent: 'center', alignItems: 'center'}} to="/take-photo">Take Photo</Link>
+		</div>
+	)
 }
 
 const App = () => {
